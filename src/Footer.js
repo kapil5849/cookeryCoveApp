61822a40fd1ef0ecc10b34c2421b2dae502fd3f9
kapil vaishnav <kvaishnav408@gmail.com>
2023-02-24 19:33:17 +0530

    code for cookery cove

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <p className="footer-text">Developed by 💜  <a href='https://twitter.com/kapil__vaishnav'>Kapil vaishnav</a></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
